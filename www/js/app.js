3ac3a0efb313ab52ac24e3e80c70b052730f34b8
mlefree <mathieu.leprevost@apps4pro.com>
2014-05-27 00:30:36 +0200

    MLE: refactor with bower

---
+++ www/js/app.js
@@ -0,0 +1,15 @@
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myNatiApp', [
+  'ngRoute',
+  'myNatiApp.filters',
+  'myNatiApp.services',
+  'myNatiApp.directives',
+  'myNatiApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/help', {templateUrl: 'partials/help.html', controller: 'CtrlHelp'});
+  $routeProvider.when('/disk', {templateUrl: 'partials/disk.html', controller: 'CtrlDisk'});
+  $routeProvider.otherwise({redirectTo: '/disk'});
+}]);
